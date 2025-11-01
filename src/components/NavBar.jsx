81d2cdc246f23c99051e7af951d3452efc1f809e
mahat issack <mohamedissac2024@gmail.com>
2025-11-01 19:22:43 +0000

    complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
+// src/components/NavBar.js
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
     </nav>
-    );
-};
+  );
+}
 
 export default NavBar;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,30 @@
-import { useEffect, useState } from "react";
+// src/pages/Movie.js
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then((res) => res.json())
+      .then(setMovie);
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>{movie.title}</h1>
+      <p>{movie.time}</p>
+      {movie.genres.map((genre, index) => (
+        <span key={index}>{genre} </span>
+      ))}
+    </div>
   );
-};
+}
 
 export default Movie;
